3c68d1f16c3b2c59e808e9397125a345b4283309
musson <mussontmn@gmail.com>
2023-06-10 12:01:27 +0300

    8th step done

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -1,16 +1,20 @@
+import { useTranslation } from 'react-i18next';
 import getRoute from '../routes.js';
 import pathToErrorImg from '../assets/404notFound.svg';
 
-const ErrorPage = () => (
-  <div className="text-center">
-    <img alt="Страница не найдена" className="img-fluid h-25" src={pathToErrorImg} />
-    <h1 className="h4 text-muted">Страница не найдена</h1>
-    <p className="text-muted">
-      Но вы можете перейти
-      {' '}
-      <a href={getRoute.chatPagePath()}>на главную страницу</a>
-    </p>
-  </div>
-);
+const ErrorPage = () => {
+  const { t } = useTranslation();
 
+  return (
+    <div className="text-center">
+      <img alt="Страница не найдена" className="img-fluid h-25" src={pathToErrorImg} />
+      <h1 className="h4 text-muted">{t('errorPage.notFound')}</h1>
+      <p className="text-muted">
+        {t('errorPage.redirect')}
+        {' '}
+        <a href={getRoute.chatPagePath()}>{t('errorPage.toMainPage')}</a>
+      </p>
+    </div>
+  );
+};
 export default ErrorPage;
